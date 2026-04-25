48dbb498f63e6de328515ef50fa98b671c11827c
Haile <hailecst@gmail.com>
2026-04-25 16:56:38 +0300

    event loop implemented

--- internal/chatroom/run.go
+++ internal/chatroom/run.go
@@ -2,6 +2,10 @@ package chatroom
 
 import (
 	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -52,4 +56,85 @@ func (cr *ChatRoom) periodicSnapshots() {
 			}
 		}
 	}
+}
+
+func (cr *ChatRoom) Run() {
+
+	fmt.Println("ChatRoom heartbeat started...")
+	go cr.cleanupInactiveClients()
+
+	for {
+		select {
+
+		case client := <-cr.join:
+			cr.handleJoin(client)
+
+		case client := <-cr.leave:
+			cr.handleLeave(client)
+
+		case message := <-cr.broadcast:
+			cr.handleBroadcast(message)
+
+		case client := <-cr.listUsers:
+			cr.sendUserList(client)
+
+		case dm := <-cr.directMessage:
+			cr.handleDirectMessage(dm)
+		}
+	}
+}
+
+func runServer() {
+
+	chatRoom, err := NewChatRoom("./chatdata")
+	if err != nil {
+		fmt.Printf("Failed to initialize: %v\n", err)
+		return
+	}
+	defer chatRoom.shutdown()
+
+	// Set up signal handling for graceful shutdown
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		fmt.Println("\n Received shutdown signal")
+		chatRoom.shutdown()
+		os.Exit(0)
+	}()
+
+	go chatRoom.Run()
+
+	listener, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server started on :9000")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		fmt.Println("New connection from:", conn.RemoteAddr())
+		go handleClient(conn, chatRoom)
+	}
+}
+
+func (cr *ChatRoom) shutdown() {
+	fmt.Println("\n Shutting down...")
+	if err := cr.createSnapshot(); err != nil {
+		fmt.Printf("Final snapshot failed: %v \n", err)
+	}
+
+	if cr.walFile != nil {
+		cr.walFile.Close()
+	}
+
+	fmt.Println("Shutdown complete")
 }
